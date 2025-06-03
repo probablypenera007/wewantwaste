d5d9483d8e6c384c6094aca23b2cfdddec4f2aeb
James Penera <jamespenera@icloud.com>
2025-06-03 23:09:27 +0400

    iamges

---
+++ src/assets/index.js
@@ -0,0 +1,21 @@
+import skip4 from "./www1.png"
+import skip6 from "./www2.png"
+import skip8 from "./www3.png"
+import skip10 from "./www4.png"
+import skip12 from "./www5.png"
+import skip14 from "./www6.png"
+import skip16 from "./www1.png";
+import skip20 from "./www2.png";
+import skip40 from "./www3.png";
+
+export const skipImages = {
+    4: skip4,
+    6: skip6,
+    8: skip8,
+    10: skip10,
+    12: skip12,
+    14: skip14,
+    16: skip16,
+    20: skip20,
+    40: skip40,
+}
